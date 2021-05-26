575eed865d007fa60a867ceed482247b0d2f57f9
Ted Irland <irlandth@gmail.com>
2021-05-26 14:56:27 -0500

    Used the event emission framework to take a video that has been clicked on in the videoitem component and pass it up the DOM tree to the app component. In the App component, we store it to a newly created data element called selectedVideo and pass the video as a prop to the video detail component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="container">
 <SearchBar @termChange="onTermChange"> </SearchBar>
-<VideoList :videos="videos"> </VideoList>
+<VideoDetail :video="selectedVideo" />
+<VideoList @videoSelect="onVideoSelect" :videos="videos"> </VideoList>
      </div>
 </template>
 
@@ -10,18 +11,23 @@
 import axios from 'axios'
 import SearchBar from './components/SearchBar';
 import VideoList from './components/VideoList';
+import VideoDetail from './components/VideoDetail';
 import {YOUTUBE_API_KEY} from '../config'
 
 export default {
   name: 'App',
   components: {
     SearchBar,
-    VideoList
+    VideoList,
+    VideoDetail
   },
   data() {
-    return { videos: []}
+    return { videos: [], selectedVideo: null}
   },
   methods:{
+    onVideoSelect(video) {
+      this.selectedVideo = video;
+    },
     onTermChange(searchTerm) {
       axios.get('https://www.googleapis.com/youtube/v3/search', {
         params: {
