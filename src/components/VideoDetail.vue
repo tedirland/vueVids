af47e1de162c86f413b4733a30b3590e3e5db0ed
Ted Irland <irlandth@gmail.com>
2021-05-26 15:36:13 -0500

    Styled the video details and iframe to appear in a 16x9 container. Also used bootstrap column layout to align the video details in a md-8 col and the list in a md-4 col.

--- src/components/VideoDetail.vue
+++ src/components/VideoDetail.vue
@@ -1,6 +1,9 @@
 <template>
 
-<div v-if="video">
+<div v-if="video" class="col-md-8">
+  <div class="ratio ratio-16x9">
+    <iframe :src="videoUrl" />
+     </div>
   <div class="details">
  <h4>  {{video.snippet.title}} </h4>
  <p>{{video.snippet.description}}</p>
@@ -11,7 +14,14 @@
 <script>
 export default {
  name: 'VideoDetail',
- props: ['video']
+ props: ['video'],
+ computed: {
+   videoUrl() {
+     const {videoId} = this.video.id;
+     return `https://www.youtube.com/embed/${videoId} `
+
+   }
+ }
 }
 </script>
 
--- src/components/VideoList.vue
+++ src/components/VideoList.vue
@@ -1,6 +1,6 @@
 <template>
 
-<ul class="list-group">
+<ul class="list-group col-md-4">
  <VideoListItem
  v-for="video in videos"
  :video="video"
